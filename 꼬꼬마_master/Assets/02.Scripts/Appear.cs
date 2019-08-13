e305863bc9e63d80b8e85933398a68445d351960
Flower2ree <53861134+Flower2ree@users.noreply.github.com>
2019-08-14 02:16:41 +0900

    이상현상
    랜덤구현

---
+++ 꼬꼬마_master/Assets/02.Scripts/Appear.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appear : MonoBehaviour
+{
+    public GameObject[] spots;
+    int count = 0, k;
+    int[] array= new int[100];
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Choose");
+
+    
+    }
+
+    IEnumerator Choose()
+
+    {
+        while (count<7)
+
+        {
+            if (count < 3)
+            {
+                yield return new WaitForSeconds(3f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(1f);
+            }
+                k = Random.Range(0, spots.Length);
+                GameObject pt = spots[k];
+                if (array[k] != 1)
+                {
+                    pt.GetComponent<Onoff>().Change();
+                    array[k] = 1;
+                    count++;
+                }
+            
+        }
+
+    }
+
+
+    // Update is called once per frame
+  
+}
---
+++ 꼬꼬마_master/Assets/02.Scripts/Onoff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onoff : MonoBehaviour
+{
+    public bool On = false;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+    if(On)
+        {
+            Debug.Log("On");
+            Debug.Log(this);
+        }
+    }
+    public void  Change()
+    {
+        On = !On;
+    }
+}
