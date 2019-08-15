d51698b29b77230acd3f26539ec99dc20151c669
lee <totolikid@naver.com>
2019-08-15 13:19:14 +0900

    자잘한 수정
    귀신 시아 확장 및 수정
    플레이어와 귀신 속도 조절
    이상현상 생성 방법이 쪼금 문제가... 그래서 수정함....

--- 꼬꼬마_master/Assets/02.Scripts/Appear.cs
+++ 꼬꼬마_master/Assets/02.Scripts/Appear.cs
@@ -5,44 +5,55 @@ using UnityEngine;
 public class Appear : MonoBehaviour
 {
     public GameObject[] spots;
-    int count = 0, k;
-    int[] array= new int[100];
+    public int count = 0, k;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("Choose");
-
     
     }
 
-    IEnumerator Choose()
-
+    void Update()
     {
-        while (count<7)
+        
+    }
+
+    IEnumerator Choose()
+    {
+        while (true)
 
         {
             if (count < 3)
             {
                 yield return new WaitForSeconds(3f);
             }
-            else
+            else if(count<spots.Length-1)
             {
                 yield return new WaitForSeconds(1f);
             }
-                k = Random.Range(0, spots.Length);
-                GameObject pt = spots[k];
-                if (array[k] != 1)
-                {
-                    pt.GetComponent<Onoff>().Change();
-                    array[k] = 1;
-                    count++;
-                }
+            else
+            {
+                yield return new WaitForSeconds(1f);
+
+                continue;
+            }
+
+            k = Random.Range(0, spots.Length-1);
+            GameObject CheckingSpot=spots[k];
+
+            while (CheckingSpot.GetComponent<Onoff>().On != false)
+            {
+                k = Random.Range(0, spots.Length - 1);
+                CheckingSpot = spots[k];
+            }
+
+            CheckingSpot.GetComponent<Onoff>().Change();
+            Debug.Log("Changed");
+            count++;
             
         }
 
     }
-
-
-    // Update is called once per frame
   
 }
--- 꼬꼬마_master/Assets/02.Scripts/GhostSightScript.cs
+++ 꼬꼬마_master/Assets/02.Scripts/GhostSightScript.cs
@@ -9,13 +9,17 @@ public class GhostSightScript : MonoBehaviour
 
     public float SightRange = 8f;
     public float SightshortRange = 4f;
+    public float SightDis = 6f;//원형 시아 중심을 앞으로 보내 진짜 시아처럼 만들기 위한 변수
 
     public float NormalSpeed = 2f;
+    public float FollowSpeed = 2f;
     public float MaxSpeed = 4f;
 
     public GameObject Player;
 
     public NavMeshAgent agent;
+
+    public Transform GhostHeadPos;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +27,13 @@ public class GhostSightScript : MonoBehaviour
         IsInSight = false;
         Player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = NormalSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SearchInSight_1() && SearchInSight_2())
+        if (SearchInSight_1() && SearchInSight_2())//플레이어가 시아에 있을때
         {
             IsInSight = true;
 
@@ -38,10 +43,13 @@ public class GhostSightScript : MonoBehaviour
 
             Debug.Log("Player in");
 
-        }else if (IsInSight)
+        }else if (IsInSight)//플레이어가 시아에서 없어졌을때
         {
             IsInSight = false;
 
+            agent.speed = NormalSpeed;//기본속도로 설정
+
+            //다른 경로 찾기
             int temppath = Random.Range(0, GetComponent<test>().pathnum - 1);
             GetComponent<test>().paths = temppath;
             GetComponent<test>().TargetPos = GetComponent<test>().targets[temppath].transform;
@@ -50,10 +58,16 @@ public class GhostSightScript : MonoBehaviour
         }
     }
 
-    bool SearchInSight_1()
+    bool SearchInSight_1()//플레이어가 시아 콜라이더에 있는지 확인
     {
-        Collider[] colls = Physics.OverlapSphere(transform.position, SightRange);
+        Collider[] colls = Physics.OverlapSphere(GhostHeadPos.position+Vector3.forward*SightDis, SightRange);
         foreach(Collider coll in colls)
+        {
+            if (coll.tag == "Player")
+                return true;
+        }
+        colls = Physics.OverlapSphere(GhostHeadPos.position, 4f);//시아에 별개로 플레이어가 가까이 있을때를 확인
+        foreach (Collider coll in colls)
         {
             if (coll.tag == "Player")
                 return true;
@@ -63,7 +77,7 @@ public class GhostSightScript : MonoBehaviour
 
     }
 
-    bool SearchInSight_2()
+    bool SearchInSight_2()//플레이어가 가려져 있는지 확인
     {
         Vector3 Dir;
         Dir = (Player.transform.position - this.transform.position).normalized;
@@ -81,11 +95,11 @@ public class GhostSightScript : MonoBehaviour
         return false;
     }
 
-    void SpeedSet()
+    void SpeedSet()//시야 조절 함수
     {
         float Dis = Vector3.Distance(transform.position, Player.transform.position);
 
         if (Dis <= SightshortRange) agent.speed = Mathf.Lerp(agent.speed, MaxSpeed, Time.deltaTime);
-        else if (Dis <= SightRange) agent.speed = Mathf.Lerp(agent.speed, NormalSpeed, Time.deltaTime);
+        else if (Dis <= SightRange) agent.speed = Mathf.Lerp(agent.speed, FollowSpeed, Time.deltaTime);
     }
 }
