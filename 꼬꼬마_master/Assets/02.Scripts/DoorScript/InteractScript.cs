b24deae0566ac8b586a62b8aa68cd6574d24e122
S2historymakerS2 <48664307+S2historymakerS2@users.noreply.github.com>
2019-08-16 15:37:03 +0900

    문 재수수수정

--- 꼬꼬마_master/Assets/02.Scripts/DoorScript/InteractScript.cs
+++ 꼬꼬마_master/Assets/02.Scripts/DoorScript/InteractScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class InteractScript : MonoBehaviour
 {
     public float interactDistance = 5f;
     void Start()
