6226260820a5ef8252a7d0913877728e37aba111
Leeslave <54054162+Leeslave@users.noreply.github.com>
2019-08-20 23:05:59 +0900

    삼혐

---
+++ 꼬꼬마_master/Assets/02.Scripts/CameraText/CameraText1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraText1 : MonoBehaviour
+{
+    public Text CameraText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraText.text = "1F_Living_Room";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
