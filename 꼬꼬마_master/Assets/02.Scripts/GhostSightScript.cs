75a88507d6bac9d4845289a716c9a10c7ff9fe6f
lee <totolikid@naver.com>
2019-08-20 14:09:24 +0900

    귀신 시야 수정
    콜라이더 말고 시야각을 이용한 시야 확인
    플레이어가 시야에서 없어졌을경우 귀신은 플레이어와 가장 가까운 경로로 이동한다

--- 꼬꼬마_master/Assets/02.Scripts/GhostSightScript.cs
+++ 꼬꼬마_master/Assets/02.Scripts/GhostSightScript.cs
@@ -7,6 +7,7 @@ public class GhostSightScript : MonoBehaviour
 {
     public bool IsInSight;
 
+    public float SightAngle=60f;
     public float SightRange = 8f;
     public float SightshortRange = 4f;
     public float SightDis = 6f;//원형 시아 중심을 앞으로 보내 진짜 시아처럼 만들기 위한 변수
@@ -14,12 +15,15 @@ public class GhostSightScript : MonoBehaviour
     public float NormalSpeed = 2f;
     public float FollowSpeed = 2f;
     public float MaxSpeed = 4f;
+    
 
     public GameObject Player;
 
     public NavMeshAgent agent;
 
     public Transform GhostHeadPos;
+
+    Vector3 Dir;//플레이어와 귀신의 방향
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +37,9 @@ public class GhostSightScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        Dir = (Player.transform.position - GhostHeadPos.position).normalized;
+
         if (SearchInSight_1() && SearchInSight_2())//플레이어가 시아에 있을때
         {
             IsInSight = true;
@@ -49,10 +56,21 @@ public class GhostSightScript : MonoBehaviour
 
             agent.speed = NormalSpeed;//기본속도로 설정
 
-            //다른 경로 찾기
-            int temppath = Random.Range(0, GetComponent<test>().pathnum - 1);
-            GetComponent<test>().paths = temppath;
-            GetComponent<test>().TargetPos = GetComponent<test>().targets[temppath].transform;
+            //플레이어와 가장 가까운 경로 찾기
+            float pathDis = 10000f;
+            int tempPath = 0;
+            
+
+            for(int i = 0; i < GetComponent<test>().pathnum-1; i++)
+            {
+                if((Player.transform.position- GetComponent<test>().targets[i].transform.position).sqrMagnitude < pathDis)
+                {
+                    tempPath = i;
+                    pathDis = (Player.transform.position - GetComponent<test>().targets[i].transform.position).sqrMagnitude;
+                }
+            }
+            GetComponent<test>().paths = tempPath;
+            GetComponent<test>().TargetPos = GetComponent<test>().targets[tempPath].transform;
 
             Debug.Log("Player out");
         }
@@ -60,13 +78,16 @@ public class GhostSightScript : MonoBehaviour
 
     bool SearchInSight_1()//플레이어가 시아 콜라이더에 있는지 확인
     {
-        Collider[] colls = Physics.OverlapSphere(GhostHeadPos.position+Vector3.forward*SightDis, SightRange);
-        foreach(Collider coll in colls)
-        {
-            if (coll.tag == "Player")
-                return true;
-        }
-        colls = Physics.OverlapSphere(GhostHeadPos.position, 4f);//시아에 별개로 플레이어가 가까이 있을때를 확인
+        //시야각 사용하여 시야에 있는지 확인
+        float Dot = Vector3.Dot(Dir, transform.forward);
+        float Angle = Mathf.Acos(Dot)*Mathf.Rad2Deg;
+
+        if (Angle < SightAngle) return true;
+
+
+        //시아에 별개로 플레이어가 가까이 있을때를 확인
+        Collider[] colls;
+        colls = Physics.OverlapSphere(GhostHeadPos.position, 8f);
         foreach (Collider coll in colls)
         {
             if (coll.tag == "Player")
@@ -79,12 +100,12 @@ public class GhostSightScript : MonoBehaviour
 
     bool SearchInSight_2()//플레이어가 가려져 있는지 확인
     {
-        Vector3 Dir;
-        Dir = (Player.transform.position - this.transform.position).normalized;
+        
             
         RaycastHit hit;
-        if(Physics.Raycast(transform.position,Dir,out hit, SightRange))
+        if(Physics.Raycast(GhostHeadPos.position,Dir,out hit, 50f))
         {
+            //Debug.Log("tag = " + hit.collider.tag);
             //Gizmos.color = Color.red;
            // Gizmos.DrawRay(transform.position, Dir * hit.distance);
 
