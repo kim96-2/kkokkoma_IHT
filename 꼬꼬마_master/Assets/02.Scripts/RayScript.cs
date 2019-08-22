a74dcc0ad42411314ad4ef496b2bc390c57c5701
S2historymakerS2 <48664307+S2historymakerS2@users.noreply.github.com>
2019-08-23 04:10:04 +0900

    Ray 상호작용(임시 F누르,면 물체가 빨간색으로 변함)

---
+++ 꼬꼬마_master/Assets/02.Scripts/RayScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayScript : MonoBehaviour
+{
+
+    RaycastHit hit;
+    public float MaxDistance = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.DrawRay(transform.position, transform.forward * MaxDistance, Color.blue, 0.3f);
+            if(Physics.Raycast(transform.position, transform.forward, out hit, MaxDistance))
+            {
+                // 앞에 닿으면 F로 상호작용할 수 있음
+
+                hit.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            }
+
+        }
+   
+    }
+}
