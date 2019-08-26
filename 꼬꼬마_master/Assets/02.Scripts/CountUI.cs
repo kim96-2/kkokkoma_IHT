c16ab347967547d36e04022e3a314cea82345b5f
S2historymakerS2 <48664307+S2historymakerS2@users.noreply.github.com>
2019-08-27 02:11:57 +0900

    Key UI

---
+++ 꼬꼬마_master/Assets/02.Scripts/CountUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUI : MonoBehaviour
+{
+    public int count = 0;
+    Text CountText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountText.text = "Count is :" + count;
+        
+    }
+    void CountUp()
+    {
+        count++;
+
+    }
+
+}
