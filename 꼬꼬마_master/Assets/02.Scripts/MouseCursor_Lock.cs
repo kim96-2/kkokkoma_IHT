41f4aa721b0021125bc2d68fe47ef79ac22af5d0
S2historymakerS2 <48664307+S2historymakerS2@users.noreply.github.com>
2019-08-27 01:34:00 +0900

    마우스 커서 동결 p c누르면 다시 생김

--- 꼬꼬마_master/Assets/02.Scripts/MouseCursor_Lock.cs
+++ 꼬꼬마_master/Assets/02.Scripts/MouseCursor_Lock.cs
@@ -7,12 +7,22 @@ public class MouseCursor_Lock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerController.Player_Convert == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 }
